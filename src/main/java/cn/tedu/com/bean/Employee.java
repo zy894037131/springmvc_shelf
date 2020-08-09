94907a54376a940dbb1b3d5aa05ec7df4c88a3c3
zhaoy <894037131@qq.com>
2020-08-09 19:10:52 +0800

    idea搭建ssm框架时,会确实aop相关jar包org.aspectj,使用aop控制事务启动报错找不到类

--- src/main/java/cn/tedu/com/bean/Employee.java
+++ src/main/java/cn/tedu/com/bean/Employee.java
@@ -9,6 +9,7 @@ public class Employee implements Serializable {
     private String lastName;
     private String gender;
     private String email;
+    private Department department;
 
     public Employee() {
         super();
@@ -21,46 +22,13 @@ public class Employee implements Serializable {
         this.email = email;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
+    public Employee(Integer id, String lastName, String gender, String email, Department department) {
         this.id = id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
         this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
+        this.department = department;
     }
 
 
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", lastName='" + lastName + '\'' +
-                ", gender='" + gender + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
